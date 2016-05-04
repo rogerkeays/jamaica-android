35dd4bf38599866e37bf9952bc174366dba38f2b
Roger Keays <roger.keays@ninthavenue.com.au>
2016-05-04 19:06:36 +0100

    encapsulate create_preference_link

--- src/uicompositions/create_preference_link.java
+++ src/uicompositions/create_preference_link.java
@@ -2,10 +2,8 @@ package jamaica.android.uicompositions;
 
 import android.content.Context;
 import android.net.Uri;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
+import android.preference.Preference;
 import android.preference.PreferenceScreen;
-import android.preference.PreferenceManager;
 import android.content.Intent;
 import jamaica.core.testing.TestGrouper.UILayer;
 
@@ -15,11 +13,10 @@ public class create_preference_link implements UILayer {
      * Create a PreferenceScreen object that acts as a link with the given
      * title and summary.
      */
-    public static PreferenceScreen create_preference_link(
+    public static Preference create_preference_link(
                 PreferenceScreen root, String url,
                 int title_resource, int summary_resource) {
-        PreferenceScreen link = root.getPreferenceManager()
-                .createPreferenceScreen(root.getContext());
+        Preference link = new Preference(root.getContext());
         link.setIntent(new Intent().setAction(Intent.ACTION_VIEW)
                 .setData(Uri.parse(url)));
         link.setTitle(title_resource);
